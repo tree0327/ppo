00d3f193299bcca48155147b4a777f53682979a4
김다빈 <00ekqls00@naver.com>
2025-08-20 23:59:29 +0900

    포트폴리오 작업

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(function() {
     gsap.to(window,0.2,{scrollTo:0})
 
-
     var intro = gsap.timeline({
         onComplete:function(){
-            gsap.set('body',{'overflow':'auto'})
+            // 애니메이션 완료 후 FullPage 초기화하여 스크롤 활성화
+            initFullpage();
         }
         
     });
@@ -18,12 +18,10 @@ $(function() {
     .to('.sc_intro .sc_title span:nth-child(4)',0.5,{opacity:1})
     .to('.sc_intro .sc_title span:nth-child(4)',0.5,{'letter-spacing':'3vw'})
     .addLabel('m1')
-    .to(window,1.5,{scrollTo:'.sc_works'},'m1')
+    // 자동 스크롤 부분 제거
     .to('.sc_intro .sc_title span:nth-child(4)',1,{'filter':'blur(50px)'},'m1')
     .set('.sc_intro .sc_title span:nth-child(4)',{'filter':'blur(0px)'},)
 
-    
-    
     $('.nav a').click(function(e){
         e.preventDefault();
        var target = $(this).attr('href');
@@ -36,14 +34,11 @@ $(function() {
 
     })
 
-
-
     var $grid = $('.grid').isotope({
         itemSelector: '.cont',
         layoutMode: 'fitRows',
         stagger: 100
     });
-
 
     $('.filter_area').on( 'click', 'button', function() {
         var filterValue = $(this).attr('data-filter');
@@ -52,10 +47,36 @@ $(function() {
         $(this).addClass('on').siblings().removeClass('on')
     });
 
+    // Fullpage 초기화 함수
+    function initFullpage() {
+        const myFullpage = new fullpage('#fullpage', {
+            navigation: true,
+            navigationPosition: 'left',
+            showActiveTooltip: true,
+            
+            lockAnchors: true,
+            anchors: ['page01', 'page02', 'page03', 'page04'],
 
+            autoScrolling: true,
+            scrollHorizontally: true,
 
+            verticalCentered: true,
+            
+            scrollOverflow: false,
+        });
+    }
 
-
-
-
-})
+    // grow-swiper (유알스코프)
+    new Swiper(".swiper", {
+        observer: true,
+        pagination: { 
+            el: '.swiper-pagination' 
+        },
+        navigation: { 
+            nextEl: '.swiper-button-next', 
+            prevEl: '.swiper-button-prev' 
+        },
+        allowTouchMove: true,
+        spaceBetween: 30,
+    });
+});
